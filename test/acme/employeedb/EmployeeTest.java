a61e20130009d9585b86b6745d3fe3706c756aa8
Paul Cantrell <cantrell@pobox.com>
2025-10-09 10:50:43 -0500

    Added unit tests

---
+++ test/acme/employeedb/EmployeeTest.java
@@ -0,0 +1,25 @@
+package acme.employeedb;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class EmployeeTest {
+    @Test
+    void testLabTechAccess() {
+        Employee labTech = new LabTech("Sally", 1);
+        assertTrue(labTech.hasAccess(new Room(100, RoomType.LAB)));
+        assertFalse(labTech.hasAccess(new Room(100, RoomType.OFFICE)));
+        assertFalse(labTech.hasAccess(new Room(100, RoomType.SUPPLY_CLOSET)));
+    }
+
+    @Test
+    void testMiddleManagerAccess() {
+        Employee labTech = new MiddleManager("Ling", 1, 3);
+        assertTrue(labTech.hasAccess(new Room(300, RoomType.OFFICE)));
+        assertTrue(labTech.hasAccess(new Room(399, RoomType.OFFICE)));
+        assertFalse(labTech.hasAccess(new Room(400, RoomType.OFFICE)));
+        assertFalse(labTech.hasAccess(new Room(300, RoomType.LAB)));
+        assertFalse(labTech.hasAccess(new Room(300, RoomType.SUPPLY_CLOSET)));
+    }
+}
